9822916adbb3d741b6a86d15081b080cff8fbd59
Vladimir <gilgeladvladimir@gmail.com>
2018-12-25 01:00:58 +0300

    Домашнее задание первого урока.
    Все три домашних задания.

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,8 +1,20 @@
 /**
  * Created by Happy on 24.12.2018.
  */
+
 public class MainClass {
+    private int class_number = 20;
+    private String class_string = "Hello, world";
+
     public int getLocalNumber() {
         return 14;
+    }
+    public int getClassNumber() {
+        int a = this.class_number;
+        return a;
+    }
+    public String getClassString() {
+        String a = this.class_string;
+        return a;
     }
 }
